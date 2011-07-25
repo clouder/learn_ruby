7209f1c77c169fd9e76d3fdff4b9de84dbd5a878
Gumaro <clouder@clouder-kubuntu-vb.(none)>
2011-07-24 22:30:22 -0600

    04_mega_calculator specs completed and passing
    The calculator chapter has been split up into a simple and mega
    calculator.  Rewrote the pending specs and reused the code from the
    already written calculator.rb in the older learn_ruby course.

--- 04_mega_calculator/calculator_spec.rb
+++ 04_mega_calculator/calculator_spec.rb
@@ -44,17 +44,38 @@ end
 
 describe "#multiply" do
 
-  it "multiplies two numbers"
+  it "multiplies two numbers" do
+    multiply(4,10).should == 40
+    multiply(3,8).should == 24
+  end
 
-  it "multiplies an array of numbers"
+  it "multiplies an array of numbers" do
+    multiply([1,2]).should == 2
+    multiply([1,2,3]).should == 6
+    multiply([1,2,3,4]).should == 24
+  end
 
 end
 
 # http://en.wikipedia.org/wiki/Factorial
 describe "#factorial" do
-  it "computes the factorial of 0"
-  it "computes the factorial of 1"
-  it "computes the factorial of 2"
-  it "computes the factorial of 5"
-  it "computes the factorial of 10"
+  it "computes the factorial of 0" do
+    factorial(0)
+  end
+  
+  it "computes the factorial of 1" do
+    factorial(1).should == 1
+  end
+  
+  it "computes the factorial of 2" do
+    factorial(2).should == 2
+  end
+  
+  it "computes the factorial of 5" do
+    factorial(5).should == 120
+  end
+  
+  it "computes the factorial of 10" do
+    factorial(10).should == 3628800
+  end
 end
