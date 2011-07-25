4cd9e9c062cc6abb5271f003b0fee8339b33dca4
Gumaro <clouder@clouder-kubuntu-vb.(none)>
2011-07-24 22:00:34 -0600

    simon.rb now passes the latest specs
    The new simon_spec no longer requires that the methods be encapsulated
    inside a Simon module.

--- 01_simon_says/simon.rb
+++ 01_simon_says/simon.rb
@@ -1,23 +1,19 @@
-module Simon
-  
-  def echo(text)
-    text
-  end
-  
-  def shout(text)
-    text.upcase
-  end
-  
-  def repeat(text, times = 2)
-    ((text + ' ') * times).chop
-  end
-  
-  def start_of_word(word, num)
-    word[0..num-1]
-  end
-  
-  def first_word(text)
-    text.split[0]
-  end
-  
+def echo(text)
+  text
+end
+
+def shout(text)
+  text.upcase
+end
+
+def repeat(text, times = 2)
+  ((text + ' ') * times).chop
+end
+
+def start_of_word(word, num)
+  word[0..num-1]
+end
+
+def first_word(text)
+  text.split[0]
 end
