687a7a3ca8fda0efdc4b56b76a764c065c8f9264
Gumaro <clouder@clouder-kubuntu-vb.(none)>
2011-08-06 13:25:46 -0600

    More flexible solution for 16_in_words
    Removed all the brittle if statements for a more concise and flexible
    solution. This solution requires that you only need add the word to
    the BIGS array to count in higher powers of one thousand.

--- 16_in_words/in_words.rb
+++ 16_in_words/in_words.rb
@@ -1,47 +1,35 @@
 class Fixnum
-  
-  ZERO_THRU_19 = %w{ zero one two three four five six seven eight nine
+
+  SMALLS = [''] + %w{ one two three four five six seven eight nine
 		     ten eleven twelve thirteen fourteen fifteen sixteen
-		     seventeen eighteen nineteen }     
-  TENS = [ '', '' ] + %w{ twenty thirty forty fifty sixty
-			  seventy eighty ninety }
-  
-  def in_words(num=self)
-    if num < 20
-      ZERO_THRU_19[num]
-    elsif num > 19 && num < 100
-      "#{TENS[num / 10]} " + in_words(num % 10)
-    elsif num > 99 && num < 1000
-      "#{ZERO_THRU_19[num / 100]} hundred " + in_words(num % 100)
-    elsif num > 999 && num < 1000000
-      thousands = num / 1000
-      if thousands < 20
-	"#{ZERO_THRU_19[thousands]} thousand " + in_words(num % 1000)
+		     seventeen eighteen nineteen }
+  TENS = ['', ''] + %w{ twenty thirty forty fifty sixty seventy eighty
+			  ninety }
+  BIGS = [''] + %w{ thousand million billion trillion }
+
+  def in_words()
+    return 'zero' if self == 0
+    parts = []
+
+    self.to_s.reverse.split('').each_slice(3) do |e|
+      parts << e.reverse.join
+    end
+
+    parts.each_with_index do |e, i|
+      huns = e.to_i / 100
+      tens = e.to_i % 100 / 10
+      ones = e.to_i % 10
+      parts[i] = huns > 0 ? "#{SMALLS[huns]} hundred " : ''
+      if tens > 1
+	parts[i] += "#{TENS[tens]}"
+	parts[i] += " #{SMALLS[ones]}" if ones > 0
       else
-	"#{thousands.in_words} thousand " + in_words(num % 1000)
+	parts[i] += SMALLS[tens * 10 + ones]
       end
-    elsif num > 999999 && num < 1000000000
-      millions = num / 1000000
-      if millions < 20
-	"#{ZERO_THRU_19[millions]} million " + in_words(num % 1000000)
-      else
-	"#{millions.in_words} million " + in_words(num % 1000000)
-      end
-    elsif num > 999999999 && num < 1000000000000
-      value = num / 1000000000
-      if value < 20
-	"#{ZERO_THRU_19[value]} billion " + in_words(num % 1000000000)
-      else
-	"#{value.in_words} billion " + in_words(num % 1000000000)
-      end
-    elsif num > 999999999999
-      value = num / 1000000000000
-      if value < 20
-	"#{ZERO_THRU_19[value]} trillion " + in_words(num % 1000000000000)
-      else
-	"#{value.in_words} trillion " + in_words(num % 1000000000000)
-      end
-    end.gsub(' zero', '')
+      parts[i] += " #{BIGS[i]}" if e.to_i > 0
+    end
+    parts.delete('')
+    parts.reverse.join(' ').gsub(/\s+$/, '')
   end
-  
+
 end
